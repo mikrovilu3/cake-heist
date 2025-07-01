5516fb0ce31d12ecb2ec14ad9b7022015df8307f
martiņš <martins.mikelsons7@gmail.com>
2025-07-01 16:01:07 +0300

    confeti aplication

--- Assets/Angelo Stiff/Scripts/BulletCollision.cs
+++ Assets/Angelo Stiff/Scripts/BulletCollision.cs
@@ -4,11 +4,11 @@ public class BulletCollision : MonoBehaviour
 {
     public LayerMask targetLayer; // ✅ Define which layers should destroy the bullet
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        if ((targetLayer.value & (1 << other.gameObject.layer)) != 0) // ✅ Only destroy bullet on correct collision
-        {
+        
+        
             Destroy(gameObject); // ✅ Bullet disappears only when hitting valid layers
-        }
+        
     }
 }
