798c999f57c663f826dd629685867a3c9e44b8ff
martiņš <martins.mikelsons7@gmail.com>
2025-07-04 15:07:48 +0300

    stuff

--- Assets/Angelo Stiff/Scripts/BulletCollision.cs
+++ Assets/Angelo Stiff/Scripts/BulletCollision.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using ASD;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class BulletCollision : MonoBehaviour
 {
@@ -6,9 +8,15 @@ public class BulletCollision : MonoBehaviour
     public GameObject impact;
     void OnCollisionEnter(Collision other)
     {
-        
+        if (other.gameObject.CompareTag("enemy"))
+        {
+
+            other.gameObject.transform.Rotate(new Vector3(-90,0,0));
+            other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+            other.gameObject.GetComponent<EnemyAI>().enabled = false;
+        }
             Instantiate(impact,transform.position,new Quaternion(0,0,0,0));
-            Destroy(gameObject); // ✅ Bullet disappears only when hitting valid layers
+            Destroy(gameObject); // ✅ Bullet disappears only when hitting valid layersa
         
     }
 }
