7c3bfd442d065781948f349b433fad6d2c7f09e9
martiņš <martins.mikelsons7@gmail.com>
2025-07-04 16:25:06 +0300

    menu fix

--- Assets/Angelo Stiff/Scripts/MouseLook.cs
+++ Assets/Angelo Stiff/Scripts/MouseLook.cs
@@ -7,10 +7,11 @@ public class MouseLook : MonoBehaviour
     public Transform playerBody;
     float xRotation = 0f;
     public bool isInMenu = false;
+    private MenuManager menu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        menu = GameObject.Find("Canvases").GetComponent<MenuManager>(); 
     }
 
     // Update is called once per frame
